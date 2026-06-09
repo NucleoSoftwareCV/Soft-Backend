3d5671509c48297210ceffcf4cd36e5ac2142fea
Bruno Herrera <bruno.anher@gmail.com>
2026-06-09 11:38:13 -0500

    Commit Inicial: Se genera el proyecto Base de Oona BE

---
+++ src/main/java/com/hean/consigueventas/oonabe/OonaBeApplication.java
@@ -0,0 +1,13 @@
+package com.hean.consigueventas.oonabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OonaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OonaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hean/consigueventas/oonabe/OonaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hean.consigueventas.oonabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OonaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
