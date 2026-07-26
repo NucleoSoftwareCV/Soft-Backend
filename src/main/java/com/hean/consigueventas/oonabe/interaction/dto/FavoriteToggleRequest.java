d5366f108791d079c70de9811d1c910b856e9cfd
valeriavvs <valeriavalsa2001@gmail.com>
2026-07-26 00:14:00 -0500

    Integrar perfil cliente e interacciones con develop

--- src/main/java/com/hean/consigueventas/oonabe/interaction/dto/FavoriteToggleRequest.java
+++
@@ -1,9 +0,0 @@
-package com.hean.consigueventas.oonabe.interaction.dto;
-
-import com.hean.consigueventas.oonabe.common.enums.FavoriteEntityType;
-import jakarta.validation.constraints.NotNull;
-
-public record FavoriteToggleRequest(
-    @NotNull FavoriteEntityType entityType,
-    @NotNull Long entityId
-) {}
